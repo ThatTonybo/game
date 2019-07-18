542699b34815dca444044feed68505db80d004ea
ThatTonybo <twatt74@eq.edu.au>
2019-07-19 09:45:18 +1000

    Add fullscreen hotkey

--- main.py
+++ main.py
@@ -9,9 +9,25 @@ config = context.config
 x = 1280
 y = 720
 
+state = {
+    'Fullscreen' : False
+}
+
 window.set_minimum_size(x, y)
 window.set_size(x, y)
 window.set_caption('police-game')
 window.set_visible()
+
+@window.event
+def on_key_press(symbol, modifiers):
+    if symbol == pyglet.window.key.F12:
+        if state['Fullscreen'] == False:
+            print('Switching to fullscreen mode')
+            state['Fullscreen'] = True
+            window.set_fullscreen(True)
+        else:
+            print('Switching to windowed mode')
+            state['Fullscreen'] = False
+            window.set_fullscreen(False)
 
 pyglet.app.run()
