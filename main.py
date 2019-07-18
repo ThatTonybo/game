06086ce3340ef52e59fe3305814b67ca00c12bc4
ThatTonybo <twatt74@eq.edu.au>
2019-07-19 09:36:24 +1000

    Basic window settings and sizing

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pyglet
 
-window = pyglet.window.Window()
+window = pyglet.window.Window(visible=False)
 
 context = window.context
 config = context.config
+
+x = 1280
+y = 720
+
+window.set_minimum_size(x, y)
+window.set_size(x, y)
+window.set_caption('police-game')
+window.set_visible()
 
 pyglet.app.run()
